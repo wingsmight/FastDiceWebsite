407cc5d7236b2a44b9980cc0471f35498a568152
wingsmight <wingsmight@gmail.com>
2022-01-30 23:14:46 +0700

    Updates

---
+++ precache-manifest.2231969cc99be5ebe9fe211cec10233c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d4f4651cf3177576bf163312ce613e",
+    "url": "/FastDiceWebsite/index.html"
+  },
+  {
+    "revision": "0a266704c56dd06b4314",
+    "url": "/FastDiceWebsite/static/css/main.39e0ea43.chunk.css"
+  },
+  {
+    "revision": "1b6fafc4cf0a9fbfb64a",
+    "url": "/FastDiceWebsite/static/js/2.a3f654de.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/FastDiceWebsite/static/js/2.a3f654de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a266704c56dd06b4314",
+    "url": "/FastDiceWebsite/static/js/main.e6d9ad9f.chunk.js"
+  },
+  {
+    "revision": "a4eb7132c14f53f9db21",
+    "url": "/FastDiceWebsite/static/js/runtime-main.9b7ab442.js"
+  },
+  {
+    "revision": "657a3740da8b2c23d4d8f61a955683c7",
+    "url": "/FastDiceWebsite/static/media/wm.657a3740.svg"
+  }
+]);
